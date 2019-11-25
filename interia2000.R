7d5a362b3547252b1b137a9f5aa478d3124dacb7
mw0000 <marcin@wilkowski.org>
2019-11-25 14:56:32 +0100

    get_wayback_urls

--- interia2000.R
+++ interia2000.R
@@ -83,6 +83,8 @@ write.csv(links, 'interia2000_outside.csv', fileEncoding = "UTF-8")
 
 # test
 
+library(jsonlite)
+
 avapi <- 'http://archive.org/wayback/available?url='
 
 resp <- fromJSON(paste0(avapi,links$url[3],'&timestamp=2000'))
@@ -96,15 +98,59 @@ links.lst <- as.character(links.lst)
 
 wb_links <- data.frame(stringsAsFactors = FALSE)
 
-for(l in links.lst[1:15]) {
-
-  #str(l)
-  avapi <- 'http://archive.org/wayback/available?url='
-  resp <- fromJSON(paste0(avapi,l,'&timestamp=2000'))
-  b_url = resp$archived_snapshots$closest$url
-  cat(paste0(b_url,'\n'))
-  #ifelse(!is.null(b_url), wb_url = b_url, wb_url = NULL)  
-  #wb_links <- rbind(wb_links, as.data.frame(list(url = l, wb_url), stringsAsFactors = FALSE))
+get_wayback_urls <- function(data) {
   
+  for(l in data) {
+  
+  tryCatch(
+    expr = {
+  
+      avapi <- 'http://archive.org/wayback/available?url='  
+      resp <- fromJSON(paste0(avapi,l,'&timestamp=2000'))
+
+      if(length(resp$archived_snapshots) > 0) {
+        # status odpowiedzi 200 oznacza, że strona została poprawnie zapisana w Wayback Machine
+        # wpisywanie do ramki danych odpowiedzi serwera z adresem archiwalnej kopii strony
+
+        b_url <- resp$archived_snapshots$closest$url
+        time <- resp$archived_snapshots$closest$timestamp
+        status <- resp$archived_snapshots$closest$status
+        y <- substr(time,1,4)
+        clean_url <- paste0('http://web.archive.org/web/',time,'id_/',l)
+        
+      } else {
+        # wpisywanie do ramki danych statusu odpowiedzi serwera 
+        b_url <- NA
+        time <- NA
+        status <- NA
+        y <- NA
+        clean_url <- NA
+      }
+      wb_links <- rbind(wb_links, as.data.frame(list(url = l, wb_url = b_url, wb_url_clean = clean_url, timestamp = time, year = y, http_status = status), stringsAsFactors = FALSE))
+    },
+
+    error = function(e){
+      message('Wystąpił błąd!')
+      print(e)
+      cat('-----------------------------------------\n')    
+      
+    },
+    warning = function(w){
+      message('Ostrzeżenie!')
+      print(w)
+      cat('-----------------------------------------\n')    
+      
+    },
+      finally = {
+        message('Odpowiedź serwera zapisana')
+      }
+    )    
+  }
+  return(wb_links)
 }
 
+
+
+#a <- fromJSON("https://archive.org/wayback/available?url=http://www.optel.com.pl/i&timestamp=2000")
+#a
+a <- get_wayback_urls(links.lst[130:145])
